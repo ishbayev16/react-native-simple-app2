c89fee4d733cce9719e439b73dee1faa3c64a669
anvar <ishbayev16@itu.edu.tr>
2020-03-12 00:12:41 +0300

    stack navigation

---
+++ App.js
@@ -0,0 +1,27 @@
+import React, {useState} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Home from './screens/home';
+import * as Font from 'expo-font';
+import {AppLoading} from 'expo';
+
+const getFonts = () =>{
+  return Font.loadAsync({
+    'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
+    'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf'),
+  })
+}
+
+export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+    if(fontLoaded){
+      return (
+        <Home />
+      );}else{
+        return(
+          <AppLoading 
+            startAsync={getFonts}
+            onFinish={()=>setFontLoaded(true)}
+          />
+        )
+      }
+}
