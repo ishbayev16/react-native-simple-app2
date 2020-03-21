343eff78e79ec54b9453a3a4ab16f91aa59d114c
bayramgeldi <bayramgeldi.bayriyev@gmail.com>
2020-03-22 01:30:54 +0300

    header'i bozdum :anguished:
    header'i bozdum :anguished:

--- shared/header.js
+++ shared/header.js
@@ -1,29 +1,29 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, ImageBackground } from 'react-native';
+import { StyleSheet, Text, View, Image, ImageBackground, Dimensions } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 
 export default function Header({ title, navigation }) {
 
   const openMenu = () => {
     navigation.openDrawer();
-  }
+  };
 
   return (
-    <ImageBackground source={require('../assets/game_bg.png')} style={styles.header}>
+    <View style={styles.header}>
       <MaterialIcons name='menu' size={28} onPress={openMenu} style={styles.icon} />
       <View style={styles.headerTitle}>
         <Image source={require('../assets/heart_logo.png')} style={styles.headerImage} />
         <Text style={styles.headerText}>{title}</Text>
       </View>
-    </ImageBackground>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   header: {
-    width: 320,
+    width: Dimensions.get('screen').height,
     height: '100%',
-    flexDirection: 'row',
+    backgroundColor:'rgb(31,57,113)',
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -31,7 +31,7 @@ const styles = StyleSheet.create({
     left: 40,
     fontWeight: 'bold',
     fontSize: 20,
-    color: '#333',
+    color: '#fafafa',
     letterSpacing: 1,
   },
   icon: {
